f6ff8a013de8bdbe557a05d735544d32821f8ee1
Tanishk <tanishkmeshkar471@gmail.com>
2026-02-27 18:44:18 +0530

    task2

---
+++ day1/task2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"enter a number";
+    cin>>num1;
+    cout<<"enter a number";
+    cin>>num2;
+
+    int sum=num1+num2;
+    int multiplication=num1*num2;
+    int subtraction=num1-num2;
+
+    cout<<"sum is_"<<sum<<endl;
+    cout<<"multiplication is_"<<multiplication<<endl;
+    cout<<"subtraction is_"<<subtraction<<endl;
+
+    return 0;
+}

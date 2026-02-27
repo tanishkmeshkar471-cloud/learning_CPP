7ee9c13e11fef72654d54c23b730b28465178903
Tanishk <tanishkmeshkar471@gmail.com>
2026-02-27 17:02:59 +0530

    task1

---
+++ day1/task1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    cout<<"enter your name"<<endl;
+    cin>>name;
+    int age;
+    cout<<"enter your age"<<endl;
+    cin>>age;
+
+    cout<<"after 5 years you will be_"<<age+5<<"years old"<<endl;
+}

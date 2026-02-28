28e73a4150cf2adab00951cb1bb5ebcdfe2213ee
Tanishk <tanishkmeshkar471@gmail.com>
2026-02-28 19:47:44 +0530

    solving basic problems and understand constants in c++

---
+++ Day2/area_of_rectangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int length,width;
+    cout<<"enter lenght:-"<<endl;
+    cin>>length;
+    cout<<"enter width"<<endl;
+    cin>>width;
+
+    int  area=length*width;
+    cout<<"area of a rectangle:-"<<area;
+}
---
+++ Day2/learn_constants.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int num = 15; // fix the value of num by 15
+    int num2 = 10;
+    cout << num + num2;
+}
